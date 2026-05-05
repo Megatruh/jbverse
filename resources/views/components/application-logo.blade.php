ba5c8b3f323cecfc92ec73415f2173a000cc82ca
unknown <rifkiyh6@gmail.com>
2026-05-05 13:10:43 +0700

    Navbar

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,3 +1,3 @@
-<a href="/">
+<a href="/" class="text-center">
     <img src="{{ asset('images/logo.png') }}" alt="JBVerse" class="max-w-40 max-h-40">
 </a>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,9 +16,9 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div class="bg-prim1">
+            <div>
                 <a href="/">
-                    <x-application-logo />
+                    <x-application-logo class="bg-prim1"/>
                 </a>
             </div>
 
