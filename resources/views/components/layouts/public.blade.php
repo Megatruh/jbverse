7c04f8ea3af52e1dcd15c47635186a8d41a328b4
unknown <rifkiyh6@gmail.com>
2026-05-06 18:13:31 +0700

    Fixing

--- resources/views/components/layouts/public.blade.php
+++ resources/views/components/layouts/public.blade.php
@@ -22,7 +22,8 @@
                 <div class="hidden lg:flex items-center space-x-4">
                     @guest
                         <div>
-                            <a href="{{ route('login') }}" class="mr-2 text-gray-300 hover:text-gray-100 font-medium">Masuk</a>
+                            <a href="{{ route('login') }}"
+                                class="mr-2 text-gray-300 hover:text-gray-100 font-medium">Masuk</a>
                             <a href="{{ route('register') }}"
                                 class="bg-indigo-600 text-white px-4 py-2 rounded-lg font-medium hover:bg-indigo-700 transition">Daftar</a>
                         </div>
@@ -32,7 +33,13 @@
                         @if (auth()->user()->role === 'admin')
                             <a href="/admin/dashboard" class="text-indigo-600 font-medium">Dashboard Admin</a>
                         @elseif(auth()->user()->role === 'pengusaha')
-                            <a href="/pengusaha/dashboard" class="text-indigo-600 font-medium">Toko Saya</a>
+                            <div class="flex lg:hidden items-center space-x-4">
+                                <a href="/pengusaha/dashboard" class="text-gray-500 hover:text-gray-300 text-sm font-medium">Toko Saya</a>
+                                <a href="{{ route('login') }}"
+                                    class="text-gray-500 hover:text-gray-300 font-medium text-sm">Masuk</a>
+                                <a href="{{ route('register') }}"
+                                    class="bg-indigo-600 text-white px-3 py-1.5 rounded-lg font-medium hover:bg-indigo-700 transition text-sm">Daftar</a>
+                            </div>
                         @else
                             <a href="{{ route('profile.edit') }}"
                                 class="text-gray-300 font-medium hover:text-gray-100">Profil</a>
